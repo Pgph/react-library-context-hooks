f106148613f59133cac551ef5f8acb1e00fa451f
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-14 13:24:49 +0100

    access our context data using the static property contextType

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav className="navbar">
+      <nav
+        style={{ background: theme.ui, color: theme.syntax }}
+        className="navbar"
+      >
         <h1>Context App</h1>
         <ul>
           <li>Home</li>
